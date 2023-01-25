52ec11daffc9947e5d616cd45a75ced6c898a5f1
Artem Viatkin <newyorkbox47@gmail.com>
2023-01-25 21:36:57 +0200

    Добавьте файлы проекта.

---
+++ lesson 27/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_27
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xPos, int yPos) { X = xPos; Y = yPos; }
+        public Point() { }
+
+        //переопределить Objeect.ToString()
+        public override string ToString() => $ » X = {X}; Y = {Y}»;
+        
+    }
+}
